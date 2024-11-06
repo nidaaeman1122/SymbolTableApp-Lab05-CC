b12a09e9e9c4092de1197a8d61745bd1ab3a13c6
nidaeman1122 <nidaeman0987@gmail.com>
2024-11-06 05:07:48 -0800

    Add project files.

---
+++ SymbolTableApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SymbolTableApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.symbolTextBox = new System.Windows.Forms.TextBox();
+            this.infoTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // symbolTextBox
+            // 
+            this.symbolTextBox.Location = new System.Drawing.Point(426, 117);
+            this.symbolTextBox.Name = "symbolTextBox";
+            this.symbolTextBox.Size = new System.Drawing.Size(100, 20);
+            this.symbolTextBox.TabIndex = 0;
+            // 
+            // infoTextBox
+            // 
+            this.infoTextBox.Location = new System.Drawing.Point(426, 169);
+            this.infoTextBox.Name = "infoTextBox";
+            this.infoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.infoTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(272, 253);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(414, 253);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(544, 253);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter a Symbol";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter the Information";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(411, 330);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(32, 13);
+            this.resultLabel.TabIndex = 50;
+            this.resultLabel.Text = "result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.infoTextBox);
+            this.Controls.Add(this.symbolTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox symbolTextBox;
+        private System.Windows.Forms.TextBox infoTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SymbolTableApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SymbolTableApp
+{
+    public partial class Form1 : Form
+    {
+        // Declare the symbol table (dictionary)
+        private Dictionary<string, string> symbolTable = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Optional: Form load event (if needed)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Any initialization code can go here
+        }
+
+        // Add button click handler
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string symbol = symbolTextBox.Text;
+            string info = infoTextBox.Text;
+
+            if (!string.IsNullOrEmpty(symbol) && !string.IsNullOrEmpty(info))
+            {
+                if (!symbolTable.ContainsKey(symbol))
+                {
+                    symbolTable[symbol] = info;
+                    resultLabel.Text = $"Symbol '{symbol}' added.";
+                }
+                else
+                {
+                    resultLabel.Text = $"Symbol '{symbol}' already exists!";
+                }
+            }
+            else
+            {
+                resultLabel.Text = "Please enter both symbol and information.";
+            }
+        }
+
+        // Search button click handler
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string symbol = symbolTextBox.Text;
+
+            if (symbolTable.ContainsKey(symbol))
+            {
+                string info = symbolTable[symbol];
+                resultLabel.Text = $"Symbol: {symbol}, Info: {info}";
+            }
+            else
+            {
+                resultLabel.Text = $"Symbol '{symbol}' not found.";
+            }
+        }
+
+        // Delete button click handler
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            string symbol = symbolTextBox.Text;
+
+            if (symbolTable.ContainsKey(symbol))
+            {
+                symbolTable.Remove(symbol);
+                resultLabel.Text = $"Symbol '{symbol}' deleted.";
+            }
+            else
+            {
+                resultLabel.Text = $"Symbol '{symbol}' not found.";
+            }
+        }
+    }
+}
